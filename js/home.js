f3573257f6b405a212e221ef25c421126b127c05
Jamshed Sifat <mdjamshedsifat@gmail.com>
2025-08-24 11:51:12 +0600

    transaction

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 const validPin =1234
-
+const transactionData =[]
 
  //function to get input values
 function getInputValueNumber(id){
@@ -45,6 +45,14 @@ document.getElementById('Add-Money-btn').addEventListener('click',function(e){
     const totalNewAvaillableBalance= addAmount + availlableBalance;
   
     setInnerText(totalNewAvaillableBalance)
+
+    const data ={
+        transactionType: 'Add Money',
+        bank: bank,
+        date: new Date().toLocaleTimeString(),
+
+    }
+    transactionData.push(data)
 })
 
 /* CashOut feature */
@@ -71,7 +79,52 @@ document.getElementById('withdrow-Money-btn').addEventListener('click',function(
      const totalNewAvaillableBalance=  availlableBalance - amount;
        
      setInnerText(totalNewAvaillableBalance)
+
+       const data ={
+        transactionType: 'cash Out',
+        bank: bank,
+        date: new Date().toLocaleTimeString()
+
+    }
+    transactionData.push(data)
+    
 })
+/* Transection Money feature */
+
+document.getElementById('transaction-btn').addEventListener('click',function(e){
+    e.preventDefault()
+   const transactionContainer = document.getElementById('transection-container')
+   transactionContainer.innerText =''
+   for(const data of transactionData){
+    const div =document.createElement('div')
+    div.innerHTML= `
+    <div class=" flex justify-between items-center max-w-lg mx-auto p-4">
+    <h1 class="font-bold">Transaction History</h1>
+    <p>View All</p>
+  </div>
+    <div class="max-w-lg mx-auto mt-4 bg-white rounded-xl  p-3 flex justify-between items-center border-b-1 border-gray-300 ">
+    <div class="flex items-center gap-4  border-gray-300 py-3">
+      <div class=" p-3 rounded-full bg-[#F4F5F7]">
+        <img class="mx-auto" src="./assets/wallet1.png" alt="">
+      </div>
+      <div class="">
+        <h1>${data.transactionType}</h1>
+        <p>${data.bank}</p>
+        <p>${data.date}</p>
+        
+      </div>
+    </div>
+      <i class="fa-solid fa-ellipsis-vertical"></i>
+  </div>
+ 
+    `
+
+   transactionContainer.appendChild(div)
+   }
+
+})
+
+
 //function to toggole
 function handleToggle(id){
     const forms = document.getElementsByClassName("form")
@@ -84,6 +137,7 @@ function handleToggle(id){
 document.getElementById('add-money-btn').addEventListener('click',function(e){
  
      handleToggle('add-money-parent')
+    document.getElementById('transection-container').style.display="none"
 
      const formBtns =document.getElementsByClassName("form-btn")
      
@@ -98,6 +152,7 @@ document.getElementById('add-money-btn').addEventListener('click',function(e){
 document.getElementById('cash-out-btn').addEventListener('click',function(){
 
         handleToggle('cash-out-parent')
+        document.getElementById('transection-container').style.display="none"
 
     const formBtns =document.getElementsByClassName('form-btn')
      
@@ -153,3 +208,21 @@ document.getElementById('pay-bill').addEventListener('click',function(){
     
     
 })
+document.getElementById('transaction-btn').addEventListener('click',function(){
+    document.getElementById('transection-container').style.display="block"
+
+     handleToggle('transaction-parent')
+    
+    
+    const formBtns =document.getElementsByClassName('form-btn')
+     
+     for(const btn of formBtns){
+        btn.classList.remove("border-[#0874f2]","bg-[#0874f20d]")
+        btn.classList.add("border-gray-300")
+     }
+     document.getElementById("transaction-btn").classList.remove("border-gray-300")
+     document.getElementById("transaction-parent").classList.add("border-[#0874f2]","bg-[#0874f20d]")
+    
+    
+})
+
